56d6ec9f601ed0296455169c4091c1a4cddbab02
younhoso <thdbsgh3443@naver.com>
2022-04-07 10:31:58 +0900

    onSubmit 써보자

--- moviepedia/src/components/ReviewForm.jsx
+++ moviepedia/src/components/ReviewForm.jsx
@@ -17,11 +17,22 @@ function ReviewForm() {
   const handleContentChange = (e) => {
     setContent(e.target.value);
   };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log({
+      title,
+      rating,
+      content,
+    });
+  };
+
   return (
-    <form action="">
+    <form onSubmit={handleSubmit}>
       <input type="text" value={title} onChange={handleTitleChange} />
       <input type="number" value={rating} onChange={handleRatingChange} />
       <textarea value={content} onChange={handleContentChange} />
+      <button type="submit">전송</button>
     </form>
   );
 }
