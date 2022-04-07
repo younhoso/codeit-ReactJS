c2527315bf125c4d5ea41991ae430a8c7387d25c
younhoso <thdbsgh3443@naver.com>
2022-04-07 12:16:32 +0900

    하나의 state로 폼 구현하기

--- moviepedia/src/components/ReviewForm.jsx
+++ moviepedia/src/components/ReviewForm.jsx
@@ -1,37 +1,42 @@
 import { useState } from "react";
 
 function ReviewForm() {
-  const [title, setTitle] = useState("");
-  const [rating, setRating] = useState(0);
-  const [content, setContent] = useState("");
+  const [values, setValues] = useState({
+    title: "",
+    rating: 0,
+    content: "",
+  });
 
-  const handleTitleChange = (e) => {
-    setTitle(e.target.value);
-  };
-
-  const handleRatingChange = (e) => {
-    const nextRating = Number(e.target.value) || 0;
-    setRating(nextRating);
-  };
-
-  const handleContentChange = (e) => {
-    setContent(e.target.value);
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    console.log(name);
+    console.log(value);
+    setValues((prevValues) => ({
+      ...prevValues,
+      [name]: value,
+    }));
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log({
-      title,
-      rating,
-      content,
-    });
+    console.log(values);
   };
 
   return (
     <form onSubmit={handleSubmit}>
-      <input type="text" value={title} onChange={handleTitleChange} />
-      <input type="number" value={rating} onChange={handleRatingChange} />
-      <textarea value={content} onChange={handleContentChange} />
+      <input
+        name="title"
+        type="text"
+        value={values.title}
+        onChange={handleChange}
+      />
+      <input
+        name="rating"
+        type="number"
+        value={values.rating}
+        onChange={handleChange}
+      />
+      <textarea name="content" value={values.content} onChange={handleChange} />
       <button type="submit">전송</button>
     </form>
   );
