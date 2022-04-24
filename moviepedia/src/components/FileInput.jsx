0b596929bb9082d240f370dde2c52102204938f0
younhoso <thdbsgh3443@naver.com>
2022-04-24 10:01:10 +0900

    파일 인풋 초기화

--- moviepedia/src/components/FileInput.jsx
+++ moviepedia/src/components/FileInput.jsx
@@ -1,10 +1,26 @@
+import {useRef} from 'react';
+
 function FileInput({ name, value, onChange }) {
+  const inputRef = useRef();
   const handleChange = (e) => {
     const nextValue = e.target.files[0];
     onChange(name, nextValue);
   };
 
-  return <input type="file" onChange={handleChange} />;
+  const handleClearClick = () => {
+    const inputNode = inputRef.current;
+    if(!inputNode) return;
+    inputNode.value = '';
+    onChange(name, value)
+  }
+
+  return (
+    <div>
+      <input type="file" onChange={handleChange} ref={inputRef}/>
+      {value && <button onClick={handleClearClick}>X</button>}
+    </div>
+  )
+  
 }
 
 export default FileInput;
