a5fa5e7b50c0eb6ec1c96db9f13a11c3371847c0
younhoso <thdbsgh3443@naver.com>
2022-04-29 14:59:24 +0900

    sort 정렬 바꾸기

--- moviepedia/src/components/App.jsx
+++ moviepedia/src/components/App.jsx
@@ -1,10 +1,22 @@
 import ReviewList from "./ReviewList";
 import items from "../mock.json";
+import { useState } from "react";
 
 function App() {
+  const [order, setOrder] = useState("createdAt");
+  const sortedItems = items.sort((a, b) => b[order] - a[order]);
+
+  const handleNewestClick = () => setOrder("createdAt");
+
+  const handleBestClick = () => setOrder("rating");
+
   return (
     <div>
-      <ReviewList items={items} />
+      <div>
+        <button onClick={handleNewestClick}>최신순</button>
+        <button onClick={handleBestClick}>베스트순</button>
+      </div>
+      <ReviewList items={sortedItems} />
     </div>
   );
 }
